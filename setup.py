d457062e26e85834b1425f7bb167553dc0d5eeff
rebzie22 <jordanguck22@gmail.com>
2025-07-23 19:16:43 -0500

    Add x.x.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setup(
     name="webhook-guardian",
-    version="0.1.0",
+    version="0.1.1",
     author="Jordan Guck",
     author_email="your.email@example.com",
     description="A security-focused Python library for validating and handling webhooks safely",
